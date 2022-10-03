6b6ebadd642c20e9ac27628867dcdb9d7cca42b1
Surbhi Sharma <ssurbhi560@gmail.com>
2022-10-03 21:28:27 +0530

    update conda env update command

--- condacolab.py
+++ condacolab.py
@@ -187,7 +187,7 @@ def install_from_url(
     if environment_file_url:
         print("📦 Updating environment using environment.yaml file...")
         _run_subprocess(
-            [f"{prefix}/bin/{conda_exe}", "env", "update", "-n", "base", "-f", environment_file_url],
+            [f"{prefix}/bin/python", "-m", "{prefix}/bin/{conda_exe}", "env", "update", "-n", "base", "-f", environment_file_url],
             "environment_file_update.log",
         )
 
